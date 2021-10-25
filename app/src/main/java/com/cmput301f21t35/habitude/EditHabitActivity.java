2f8c980182c8eaf2ad4fc6f7223050ed635065ce
chaumont-arch <chaumont@ualberta.ca>
2021-10-25 00:12:27 -0600

    rough support for firesone

--- app/src/main/java/com/cmput301f21t35/habitude/EditHabitActivity.java
+++ app/src/main/java/com/cmput301f21t35/habitude/EditHabitActivity.java
@@ -1,11 +1,15 @@
 package com.cmput301f21t35.habitude;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CalendarView;
 import android.widget.EditText;
+
+import java.util.HashMap;
 
 public class EditHabitActivity extends AppCompatActivity {
     EditText habitTitle;
@@ -13,6 +17,7 @@ public class EditHabitActivity extends AppCompatActivity {
     EditText habitPlan;
     CalendarView habitCalendar;
     Habit changingHabit; //Talk about this
+    final CollectionReference collectionReference = db.collection("Cities"); //And this
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +31,48 @@ public class EditHabitActivity extends AppCompatActivity {
     }
 
     public void doneButton(View view) {
+        HashMap<String, String> data = new HashMap<>(); //I don't think that's quite right
+        data.put("Title",changingHabit.getTitle());
+        data.put("Description",changingHabit.getDescription());
+        data.put("Plan",changingHabit.getPlan());
+        data.put("Calendar",changingHabit.getCalendar());
+
         try {
             changingHabit.setTitle(habitTitle.getText().toString());
+            data.put("Title",habitTitle.getText().toString());
         } catch (Exception ignored) {}
 
         try {
             changingHabit.setDescription(habitDescription.getText().toString());
+            data.put("Description",habitDescription.getText().toString());
         } catch (Exception ignored) {}
 
         try {
             changingHabit.setPlan(habitPlan.getText().toString());
+            data.put("Plan",habitPlan.getText().toString());
         } catch (Exception ignored) {}
 
         try {
             changingHabit.setDate(habitCalendar.getDate()); //?
+            data.put("Calendar", String.valueOf(habitCalendar.getDate())); //?  
+        } catch (Exception ignored) {}
+
+        try {
+            collectionReference
+                    .document(habitTitle.getText().toString()) //???
+                    .set(data)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d(TAG, "Data has been added successfully!");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "Data could not be added!" + e.toString());
+                        }
+                    });
         } catch (Exception ignored) {}
     }
 
