99f0164e9c2c2a4fe0ffc7bb9f2af834424a7758
E-Chiu <E-Chiu@users.noreply.github.com>
2021-11-03 16:40:44 -0600

    fixed making events show on list, added date to events

--- app/src/main/java/com/cmput301f21t35/habitude/Event.java
+++ app/src/main/java/com/cmput301f21t35/habitude/Event.java
@@ -1,5 +1,7 @@
 package com.cmput301f21t35.habitude;
 
+import java.util.Date;
+
 /*
 Class for the habit event
 
@@ -10,13 +12,15 @@ have to find a way for habit to store events
 public class Event {
     private String eventName;
     private String eventComment;
+    private Date date;
     // private String image; for image implementation later
 
     Event() {}
 
-    Event(String eventName, String eventComment) {
+    Event(String eventName, String eventComment, Date date) {
         this.eventName = eventName;
         this.eventComment = eventComment;
+        this.date = date;
     }
 
     // getters and setters
@@ -35,5 +39,13 @@ public class Event {
 
     public void setEventComment(String eventComment) {
         this.eventComment = eventComment;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 }
