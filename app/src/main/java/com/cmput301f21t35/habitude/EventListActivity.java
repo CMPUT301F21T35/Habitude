b434e79bde000e445a894f17b609e638d64abf83
E-Chiu <E-Chiu@users.noreply.github.com>
2021-11-04 17:22:08 -0600

    added firebase implementation for events

--- app/src/main/java/com/cmput301f21t35/habitude/EventListActivity.java
+++ app/src/main/java/com/cmput301f21t35/habitude/EventListActivity.java
@@ -1,28 +1,68 @@
 package com.cmput301f21t35.habitude;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class EventListActivity extends AppCompatActivity implements AddHabitEvent.OnFragmentInteractionListener {
 
     ListView eventList;
     ArrayAdapter<Event> eventArrayAdapter;
     ArrayList<Event> eventDataList;
+    String habitSrc = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null) {
+            habitSrc = extras.getString("HABITSRC"); // get name of the habit
+        }
+
+        // open database
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final CollectionReference collectionReference = db.collection("All Habits").document(habitSrc).collection("Events");
+
+        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
+                eventDataList.clear();
+                for(QueryDocumentSnapshot doc: queryDocumentSnapshots){
+                    String eventName = (String) doc.getId();
+                    String eventDate = (String) doc.getData().get("Date");
+                    String eventTime = (String) doc.getData().get("Time");
+                    String eventComment = (String) doc.getData().get("Comment");
+                    eventDataList.add(new Event(eventName,eventComment,eventDate,eventTime));
+                }
+                eventArrayAdapter.notifyDataSetChanged();
+            }
+        });
 
         eventList = findViewById(R.id.events_list);
         eventDataList = new ArrayList<>();
@@ -41,11 +81,30 @@ public class EventListActivity extends AppCompatActivity implements AddHabitEven
         });
     }
 
-    // TODO: INTEGRATE WITH FIREBASE
-
     @Override
     public void onOkPressed(Event newEvent) {
-        eventArrayAdapter.add(newEvent);
+        // open database
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final CollectionReference collectionReference = db.collection("All Habits").document(habitSrc).collection("Events");
+
+        HashMap<String, String> data = new HashMap<>();
+        data.put("Event Name", newEvent.getEventName());
+        data.put("Date", newEvent.getEventDate());
+        data.put("Comment", newEvent.getEventComment());
+
+        collectionReference.document(newEvent.getEventName()).set(data)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d(TAG, "Data has been added successfully");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Data has not been added successfully");
+                    }
+                });
         eventArrayAdapter.notifyDataSetChanged();
     }
 }
