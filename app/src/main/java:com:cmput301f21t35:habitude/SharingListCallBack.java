4710547c394840e3bf0dfebb8a8c0d4cb19cec0a
ccyao <60633684+woshiyc123@users.noreply.github.com>
2021-11-22 16:35:47 -0700

    Delete app/src/main/java:com:cmput301f21t35:habitude directory

--- app/src/main/java:com:cmput301f21t35:habitude/SharingListCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.cmput301f21t35.habitude;
-
-import java.util.ArrayList;
-
-public interface SharingListCallBack {
-    void onCallbackSuccess(ArrayList<String> dataList);
-
-    void onCallbackFailure(String reason);
-}
