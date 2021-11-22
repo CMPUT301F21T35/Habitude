00a2d8878f59ce46a44238398ca8b7f2a9ffcd09
Nathan Tang <nathan.ly.tang@gmail.com>
2021-11-21 22:18:30 -0700

    Ref: Remove unused imports

--- app/src/main/java/com/cmput301f21t35/habitude/SignUpLogInActivity.java
+++ app/src/main/java/com/cmput301f21t35/habitude/SignUpLogInActivity.java
@@ -11,19 +11,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
