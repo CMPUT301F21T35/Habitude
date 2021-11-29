3e54506622bff9673bb7379ec94519613a29ff01
Nathan Tang <nathan.ly.tang@gmail.com>
2021-11-29 04:51:23 -0700

    Style: Update layout to be similar to edit activity

--- app/src/main/java/com/cmput301f21t35/habitude/AddHabitActivity.java
+++ app/src/main/java/com/cmput301f21t35/habitude/AddHabitActivity.java
@@ -4,6 +4,8 @@ import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,6 +19,7 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -28,6 +31,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+
 /**
  * this activity is used to add new habit into the firestore database
  */
@@ -35,37 +39,41 @@ public class AddHabitActivity extends AppCompatActivity {
     private EditText habitName;
     private EditText habitReason;
     private DatePicker dateStart;
-    private CheckBox monday;
-    private CheckBox tuesday;
-    private CheckBox wednesday;
-    private CheckBox thursday;
-    private CheckBox friday;
-    private CheckBox saturday;
-    private CheckBox sunday;
+    private ToggleButton monday;
+    private ToggleButton tuesday;
+    private ToggleButton wednesday;
+    private ToggleButton thursday;
+    private ToggleButton friday;
+    private ToggleButton saturday;
+    private ToggleButton sunday;
     ArrayList<String> habitPlan = new ArrayList<>();
-    private Switch isPublic;
+    private SwitchCompat isPublic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_habit);
+
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Add Habit");
         //initilize the firestore database used for save datas
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         // collectionrefernce path to the current user's habits
         final CollectionReference collectionReference = db.collection("Users").document(user.getEmail()).collection("habits");
         // initilize the activity element by find id in layout
-        habitName = (EditText) findViewById(R.id.habitName);
-        habitReason = (EditText) findViewById(R.id.habitReason);
-        dateStart = (DatePicker) findViewById(R.id.datePicker);
-        monday = (CheckBox) findViewById(R.id.monday);
-        tuesday = (CheckBox) findViewById(R.id.tuesday);
-        wednesday = (CheckBox) findViewById(R.id.wednesday);
-        thursday = (CheckBox) findViewById(R.id.thursday);
-        friday = (CheckBox) findViewById(R.id.friday);
-        saturday = (CheckBox) findViewById(R.id.saturday);
-        sunday = (CheckBox) findViewById(R.id.sunday);
-        isPublic = (Switch) findViewById(R.id.isPublic);
+        habitName = findViewById(R.id.habitName);
+        habitReason = findViewById(R.id.habitReason);
+        dateStart = findViewById(R.id.datePicker);
+        monday = findViewById(R.id.monday);
+        tuesday = findViewById(R.id.tuesday);
+        wednesday = findViewById(R.id.wednesday);
+        thursday = findViewById(R.id.thursday);
+        friday = findViewById(R.id.friday);
+        saturday = findViewById(R.id.saturday);
+        sunday = findViewById(R.id.sunday);
+        isPublic = findViewById(R.id.isPublic);
         Button createButton = (Button) findViewById(R.id.createButton);
         Intent intent = new Intent(this, MainActivity.class);
         //set on click event for the CREATE button
@@ -74,9 +82,9 @@ public class AddHabitActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 //check the input is empty or not 
-                if (habitName.getText().length() == 0 || habitReason.getText().length() == 0  ){
+                if (habitName.getText().length() == 0 || habitReason.getText().length() == 0) {
                     Toast.makeText(AddHabitActivity.this, "One or more fields are empty", Toast.LENGTH_SHORT).show();
-                }else{
+                } else {
                     final String habitTitleName = habitName.getText().toString();
                     final String Reason = habitReason.getText().toString();
                     final String day = Integer.toString(dateStart.getDayOfMonth());
@@ -92,7 +100,7 @@ public class AddHabitActivity extends AppCompatActivity {
                     data.put("Date", habitStartDate);
                     data.put("Plan", habitPlan_final);
                     data.put("Is Public", isPublic.isChecked());
-                    data.put("Index",-1);
+                    data.put("Index", -1);
                     // set the firestore database
                     collectionReference.document(habitTitleName)
                             .set(data)
@@ -114,6 +122,7 @@ public class AddHabitActivity extends AppCompatActivity {
             }
         });
     }
+
     /**
      * add the plan date into the habitplan if the checkbox is checked
      */
@@ -122,26 +131,26 @@ public class AddHabitActivity extends AppCompatActivity {
             habitPlan.add("Sunday");
         }
 
-        if (monday.isChecked()){
+        if (monday.isChecked()) {
             habitPlan.add("Monday");
         }
 
-        if (tuesday.isChecked()){
+        if (tuesday.isChecked()) {
             habitPlan.add("Tuesday");
         }
         if (wednesday.isChecked()) {
             habitPlan.add("Wednesday");
         }
 
-        if (thursday.isChecked()){
+        if (thursday.isChecked()) {
             habitPlan.add("Thursday");
         }
 
-        if (friday.isChecked()){
+        if (friday.isChecked()) {
             habitPlan.add("Friday");
         }
 
-        if (saturday.isChecked()){
+        if (saturday.isChecked()) {
             habitPlan.add("Saturday");
         }
     }
